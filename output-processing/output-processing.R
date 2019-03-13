9848fc4a6bdc61827c9297885b40cf7b23479d1f
Lawrence <birdl@ae.ca>
2019-03-13 17:18:02 +0000

    Initial commit of output-processing script. This pulls Raven output from a specified location and plots timeseries of modelled streamflow and forcing data.

---
+++ output-processing/output-processing.R
@@ -0,0 +1,66 @@
+#################################################################################################################
+##
+## This script reads in Raven model output and generates summary plots
+##
+## Mar-11-2019 LAB
+##
+#################################################################################################################
+
+library(RavenR)
+
+location <- "/var/obwb-hydro-modelling/simulations"
+
+setwd(location)
+
+runname <- "test"
+
+
+######################################################
+##
+## Establish "outflow-plots.pdf" connection to write plots to.
+
+pdf(file.path(location, runname, "output-plots.pdf"), width = 11, height = 8.5)
+
+
+######################################################
+##
+## Read-in and plot forcings data
+
+forcings <- forcings.read(file.path(location, runname, paste(runname, "ForcingFunctions.csv", sep = "_")))
+
+forcings.plot(forcings$forcings)
+
+######################################################
+##
+## Read-in and plot hydrograph data
+
+hydrograph <- hyd.read(file.path(location, runname, paste(runname, "Hydrographs.csv", sep = "_")))
+
+subs <- colnames(hydrograph$hyd)
+
+for(i in 2:length(subs)){
+  
+  ## If 80% of values are NA, do not plot this timeseries
+  if(colMeans(is.na(hydrograph$hyd[,i])) > 0.8){
+    
+    print(paste(subs[i], "not plotted due to insufficient data (i.e., >80% NA values)...", sep = " "))
+    
+    i <- i+1
+    
+    }
+  
+  tmp <- hyd.extract(subs = subs[i], hydrograph)
+  
+  hyd.plot(tmp$sim, tmp$obs, precip = hydrograph$hyd$precip)
+
+  title(subs[i])
+}
+
+######################################################
+##
+## Close pdf connection with "output-plots.pdf"
+
+dev.off()
+
+
+
