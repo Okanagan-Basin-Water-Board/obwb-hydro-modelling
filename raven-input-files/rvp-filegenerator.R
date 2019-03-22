fbb12b525e2bc4a01dd6f74b3a338a3f418d027c
Lawrence <birdl@ae.ca>
2019-03-22 21:00:18 +0000

    rvp-filegenerator.R updated to operate with Ostrich by auto generating an Ostrich rvp.tpl template file. Also updated to include RVP.template read in, consistent with RVI.template read in approach.

--- raven-input-files/rvp-filegenerator.R
+++ raven-input-files/rvp-filegenerator.R
@@ -10,7 +10,7 @@
 ## RVH file generation as LAND_USE_CLASS AND VEG_CLASS are the same for now. However, it's built in here to allow these
 ## to be differentiated in future iterations of this code.
 
-template <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/RVP-Template.csv")
+RVP.template <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/RVP-Template.csv")
 
 soil.codes <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/soil_profile_codes.csv")
 
@@ -69,7 +69,7 @@ landuse.classes <- unique(landcover.codes[,c(3, 5:ncol(landcover.codes))])
 ##########################################################
 ## Land Use Parameters List
 
-landuse.parameters <- template[template$GROUP == "LandUseParameter", ]
+landuse.parameters <- RVP.template[RVP.template$GROUP == "LandUseParameter", ]
 
 ## convert all columns to character
 landuse.parameters[,] <- lapply(landuse.parameters[, ], as.character)
@@ -108,7 +108,7 @@ landuse.parameter.table <- matrix(nrow = length(layers), ncol = length(landuse.p
 ##########################################################
 ## Soil Parameters List
 
-soil.parameters <- template[template$GROUP == "SoilParameter", ]
+soil.parameters <- RVP.template[RVP.template$GROUP == "SoilParameter", ]
 
 ## convert all columns to character
 soil.parameters[,] <- lapply(soil.parameters[, ], as.character)
@@ -145,8 +145,7 @@ for(i in 1:length(soil.parameter.names)){
 ## Global Parameters Table:
 ##
 
-
-global <- template[template$GROUP == "GlobalParameter", c("PARAMETER", "DEFINITION", "VALUE")]
+global <- RVP.template[RVP.template$GROUP == "GlobalParameter", c("PARAMETER", "DEFINITION", "VALUE")]
 
 global$PARAMETER <- paste(":", global$PARAMETER, sep = '')
 
@@ -154,14 +153,14 @@ global$PARAMETER <- paste(":", global$PARAMETER, sep = '')
 ## Channel Parameters Table:
 ##
 
-channel <- template[template$GROUP == "ChannelParameter", ]
+channel <- RVP.template[RVP.template$GROUP == "ChannelParameter", ]
 
 surveypoints <- channel[channel$PARAMETER == "SurveyPoints", c("DEFINITION", "VALUE")]
 
-############################################################################################################################
-##
+#############################################################################################
 ## Generate *.rvp file
-############################################################################################################################
+##
+#############################################################################################
 
 RVPoutFile <- file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), paste(ws.interest, "-", run.number, ".rvp", sep = ""))
 
@@ -276,7 +275,275 @@ cat(file = RVPoutFile, append = T, sep = "",
 ##
 ############################################################################################################################
 
+if(run.ostrich == TRUE){
+
+##########################################################
+## Soil Parameters List
+
+## Test if soil.parameters has any values in CAL_MAX then it should be included for calibration.
+## If NOT all values are NA, generate soils.parameters.calibrate table and populate.
+## If all values ARE NA, 
+if(!all(is.na(soil.parameters$CAL_MAX))){
+  
+    soil.calibrate <- TRUE
+    
+
+    soil.parameters.calibrate <- RVP.template[RVP.template$GROUP == "SoilParameter", ]
+    
+    ## convert all columns to character
+    soil.parameters.calibrate[,] <- lapply(soil.parameters.calibrate[, ], as.character)
+    
+    soil.parameters.calibrate$CAL_VAR[which(!is.na(soil.parameters.calibrate$CAL_MAX))] <- paste("par", soil.parameters.calibrate$PARAMETER[which(!is.na(soil.parameters.calibrate$CAL_MAX))], soil.parameters.calibrate$DEFINITION[which(!is.na(soil.parameters.calibrate$CAL_MAX))], sep = "_")
+    
+    soil.parameters.calibrate$CAL_VAR[which(is.na(soil.parameters.calibrate$CAL_MAX))] <- as.character(soil.parameters.calibrate$VALUE[which(is.na(soil.parameters.calibrate$CAL_MAX))])
+    
+    layers <- unique(soil.parameters$DEFINITION)
+    
+    soil.parameter.table.calibrate <- matrix(nrow = length(layers), ncol = length(soil.parameter.names) + 1, NA)
+    
+    
+    for(i in 1:length(soil.parameter.names)){
+      
+      para <- soil.parameters.calibrate[soil.parameters.calibrate$PARAMETER == soil.parameter.names[i],]
+      
+      extract <- para[, c("DEFINITION", "CAL_VAR")]
+      
+      if(i == 1){
+        
+        soil.parameter.table.calibrate[1:nrow(para),1:2] <- as.matrix(extract)
+        
+      } else { 
+        soil.parameter.table.calibrate[,i+1] <- as.matrix(extract[,"CAL_VAR"])
+      }
+      
+      soil.units[i] <- unique(para[,"UNITS"])
+      
+    }
+    
+    print("One or more soil parameters will be included in the calibration...")
+
+} else {
+  
+  soil.calibrate <- FALSE
+  
+  print("No soil parameters will be included in the calibration...")
+  
+  }
 
 
+##########################################################
+## Land Use Parameters List
+
+if(!all(is.na(landuse.parameters$CAL_MAX))){
+  
+    landuse.calibrate <- TRUE
 
 
+    landuse.parameters.calibrate <- RVP.template[RVP.template$GROUP == "LandUseParameter", ]
+    
+    ## convert all columns to character
+    landuse.parameters.calibrate[,] <- lapply(landuse.parameters.calibrate[, ], as.character)
+    
+    landuse.parameters.calibrate$CAL_VAR[which(!is.na(landuse.parameters.calibrate$CAL_MAX))] <- paste("par", landuse.parameters.calibrate$PARAMETER[which(!is.na(landuse.parameters.calibrate$CAL_MAX))], landuse.parameters.calibrate$DEFINITION[which(!is.na(landuse.parameters.calibrate$CAL_MAX))], sep = "_")
+    
+    landuse.parameters.calibrate$CAL_VAR[which(is.na(landuse.parameters.calibrate$CAL_MAX))] <- as.character(landuse.parameters.calibrate$VALUE[which(is.na(landuse.parameters.calibrate$CAL_MAX))])
+    
+    layers <- unique(landuse.parameters.calibrate$DEFINITION)
+    
+    landuse.parameter.table.calibrate <- matrix(nrow = length(layers), ncol = length(landuse.parameter.names) + 1, NA)
+    
+    
+    
+    for(i in 1:length(landuse.parameter.names)){
+      
+      para <- landuse.parameters.calibrate[landuse.parameters.calibrate$PARAMETER == landuse.parameter.names[i],]
+      
+      extract <- para[, c("DEFINITION", "CAL_VAR")]
+      
+      if(i == 1){
+        
+        landuse.parameter.table.calibrate[,1:2] <- as.matrix(extract)
+        
+      } else { 
+        landuse.parameter.table.calibrate[,i+1] <- as.matrix(extract[,"CAL_VAR"])
+      }
+      
+      landuse.units[i] <- unique(para[,"UNITS"])
+      
+    }
+
+    print("One or more landuse parameters will be included in the calibration...")
+
+} else {
+  
+  landuse.calibrate <- FALSE
+  
+  print("No landuse parameters will be included in the calibration...")
+  
+  }
+
+
+##########################################################
+## Global Parameters
+
+global.parameters <- RVP.template[RVP.template$GROUP == "GlobalParameter",]
+
+if(!all(is.na(global.parameters$CAL_MAX))){
+  
+  global.calibrate <- TRUE
+
+  global.parameters.calibrate <- global.parameters
+  
+  global.parameters.calibrate$CAL_VAR[which(!is.na(global.parameters.calibrate$CAL_MAX))] <- paste("par", global.parameters.calibrate$PARAMETER[which(!is.na(global.parameters.calibrate$CAL_MAX))], global.parameters.calibrate$DEFINITION[which(!is.na(global.parameters.calibrate$CAL_MAX))], sep = "_")
+  
+  global.parameters.calibrate$CAL_VAR[which(is.na(global.parameters.calibrate$CAL_MAX))] <- as.character(global.parameters.calibrate$VALUE[which(is.na(global.parameters.calibrate$CAL_MAX))])
+
+  global.parameters.calibrate <- global.parameters.calibrate[, c("PARAMETER", "DEFINITION", "CAL_VAR")]  
+
+  global.parameters.calibrate$PARAMETER <- paste(":", global.parameters.calibrate$PARAMETER, sep = "")
+    
+  print("One or more global parameters will be included in the calibration...")
+  
+} else {
+  
+  global.calibrate <- FALSE
+  
+  print("No global parameters will be included in the calibration...")
+  
+}
+
+#############################################################################################
+## Generate *.rvp.tpl file
+##
+#############################################################################################
+
+OstrichRVPTemplateFile <- file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), paste(ws.interest, "-", run.number, ".rvp.tpl", sep = ""))
+
+
+cat(file = OstrichRVPTemplateFile, append = F, sep = "",
+    
+    "#########################################################################","\n",
+    ":FileType rvp Raven 2.8","\n",
+    "# DataType         Raven RVP file","\n",
+    ":Application       R","\n",
+    ":WrittenBy         Lawrence Bird","\n",
+    ":CreationDate  ",    paste(Sys.time()),"\n",
+    "#---------------------------------------------------------", "\n",
+    "#---------------------------------------------------------", "\n",
+    "# ---- Soil Classes --------------------------------------", "\n",
+    
+    ":SoilClasses","\n",
+    "     :Attributes,", "\n",
+    "     :Units,", "\n"
+)
+
+write.table(soil.classes, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F)
+
+cat(file = OstrichRVPTemplateFile, append = T, sep = "",
+    ":EndSoilClasses", "\n",
+    "#---------------------------------------------------------", "\n",
+    "# ---- Land Use Classes ----------------------------------", "\n",
+    ":LandUseClasses", "\n",
+    "     :Attributes,  IMPERMEABLE_FRAC, FOREST_COVERAGE", "\n",
+    "     :Units, fract,  fract", "\n"
+)
+
+write.table(landuse.classes, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F)
+
+cat(file = OstrichRVPTemplateFile, append = T, sep = "",
+    ":EndLandUseClasses", "\n",
+    "#---------------------------------------------------------", "\n",
+    "# ---- Vegetation Classes ----------------------------------", "\n",
+    ":VegetationClasses", "\n",
+    "     :Attributes,  MAX_HT, MAX_LAI,  MAX_LEAF_COND", "\n",
+    "     :Units, m,  none, mm_per_s", "\n"
+)
+
+write.table(vegetation.classes, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F)
+
+cat(file = OstrichRVPTemplateFile, append = T, sep = "",
+    ":EndVegetationClasses", "\n",
+    "#---------------------------------------------------------", "\n",
+    "# ---- Soil Profiles -------------------------------------", "\n",
+    ":SoilProfiles", "\n",
+    "# Name, # Horizons, Horizon 1, Thickness_1, Horizon 2, Thickness_2, ..., Horizon X, Thickness_X", "\n"
+)
+
+write.table(soil.profiles, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F, na = "")
+
+cat(file = OstrichRVPTemplateFile, append = T, sep = "",
+    ":EndSoilProfiles", "\n",
+    "#---------------------------------------------------------", "\n",
+    "# ---- Global Parameters ---------------------------------", "\n"
+)
+
+if(global.calibrate == TRUE){
+
+  write.table(global.parameters.calibrate, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = "\t", quote = F)
+
+  } else {
+    
+    write.table(global, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = "\t", quote = F)
+  }
+
+cat(file = OstrichRVPTemplateFile, append = T, sep = "",
+    "#---------------------------------------------------------", "\n",
+    "# ---- Land Use Parameters -------------------------------", "\n",
+    ":LandUseParameterList", "\n",
+    ":Parameters, ", paste(landuse.parameter.names, collapse = ", "), "\n",
+    ":Units, ", paste(landuse.units, collapse = ","), "\n"
+)
+
+if(landuse.calibrate == TRUE){
+    
+  write.table(landuse.parameter.table.calibrate, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F, na = "")
+
+  } else {
+  
+    write.table(landuse.parameter.table, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F, na = "")
+  }
+  
+cat(file = OstrichRVPTemplateFile, append = T, sep = "",
+    ":EndLandUseParameterList", "\n",
+    "#---------------------------------------------------------", "\n",
+    "# ---- Soil Parameters -----------------------------------", "\n",
+    ":SoilParameterList", "\n",
+    ":Parameters, ", paste(soil.parameter.names, collapse = ", "), "\n",
+    ":Units, ", paste(soil.units, collapse = ", "), "\n"
+)
+
+if(soil.calibrate == TRUE){
+  
+  write.table(soil.parameter.table.calibrate, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F, na = "")
+  
+  } else {
+    
+  write.table(soil.parameter.table, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F, na = "")
+    
+  }
+
+
+cat(file = OstrichRVPTemplateFile, append = T, sep = "",    
+    ":EndSoilParameterList", "\n",
+    "#---------------------------------------------------------", "\n",
+    "# ---- Channel Profiles ----------------------------------", "\n",
+    ":ChannelProfile  ", paste(channel[channel$PARAMETER == "ChannelProfile", "VALUE"]), "\n",
+    ":Bedslope  ", paste(channel[channel$PARAMETER == "Bedslope", "VALUE"]), "\n",
+    ":SurveyPoints", "\n"
+)
+
+write.table(surveypoints, OstrichRVPTemplateFile, append = T, col.names = F, row.names = F, sep = "\t", quote = F)
+
+
+cat(file = OstrichRVPTemplateFile, append = T, sep = "",
+    ":EndSurveyPoints", "\n",
+    "\n",
+    ":RoughnessZones", "\n",
+    paste(channel[channel$PARAMETER == "RoughnessZones", "DEFINITION"]), "\t",
+    paste(channel[channel$PARAMETER == "RoughnessZones", "VALUE"]), "\n",
+    ":EndRoughnessZones", "\n",
+    ":EndChannelProfile", "\n"
+)
+
+
+} else {print("No Ostrich files are being generated for this model run...")}
