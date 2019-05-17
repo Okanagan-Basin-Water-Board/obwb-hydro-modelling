9f4ef1d40e4c28a0d9969f3fc025833ce85df4fc
birdl@ae.ca <fykej@ae.ca>
2019-05-17 21:54:17 +0000

    Updated lai processing script which successfully computes maximum LAI and monthly fractional reduction for each vegetation class included in the model.

--- hru-generation/lai-processing.R
+++ hru-generation/lai-processing.R
@@ -7,59 +7,7 @@
 library(raster)
 library(sf)
 library(plyr)
-
-
-bc.albers <- "+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +datum=NAD83 +units=m +no_defs"
-
-LAI <- raster("/var/obwb-hydro-modelling/input-data/raw/spatial/GEE-LAI/reproject_1.tif", crs = bc.albers)
-
-landcover <- raster("/var/obwb-hydro-modelling/input-data/raw/spatial/EOSD_alb_Snap.tif", crs = bc.albers)
-
-vegetation.codes <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/vegetation_codes.csv")
-
-model.watersheds <- st_read("/var/obwb-hydro-modelling/input-data/raw/spatial/EFN_WS.shp", crs = bc.albers)
-
-## New version of raster package does not seem to support "sf" objects for cropping/masking. So shapefile must be converted to spatialpolygon
-model.watersheds.shape <- as(model.watersheds, "Spatial")
-
-## crop lai and landcover datasets to model watersheds so coordinates are consistent in both dataframes
-LAI.ok <- mask(crop(LAI, model.watersheds.shape), model.watersheds.shape)
-
-landcover.ok <- mask(crop(landcover, model.watersheds.shape), model.watersheds.shape)
-
-
-## extract coordinates, lai, and landcover values
-coords <- coordinates(landcover.ok)
-
-lai <- values(LAI.ok)
-
-landcover <- values(landcover.ok)
-
-## attach lai and landcover values to coordinates so all data in one dataframe
-data <- data.frame(coords, lai, landcover)
-
-## Calculate mean lai for each landcover value - na.rm must be used as there are some cells with no lai data available, but landcover data present (i.e., around lake edges etc.). LAI is calculated using all available cells for each landcover
-x <- ddply(data, .(landcover), summarize, mean = mean(lai, na.rm = T))
-
-## link vegetation bin to landcover values in resultant dataframe
-for(i in 1:nrow(x)){
-  x[i, "bin"] <- ifelse(is.na(x[i, "landcover"]), "[NONE]", as.character(vegetation.codes$Bin_type)[x[i, "landcover"] == vegetation.codes$Value])
-}
-
-
-plot(LAI.ok)
-plot(model.watersheds.shape, add = T)
-plot(landcover.ok)
-
-# for(i in 1:nrow(landcover.df)){
-# landcover.df[i, "bin"] <- ifelse(is.na(landcover.df[i, "landcover"]), "[NONE]", as.character(vegetation.codes$Bin_type)[landcover.df[i, "bin"] == vegetation.codes$Value])
-# print(i)
-# }
-# 
-# 
-# HRU.output[i, "VEG_CLASS"] <- ifelse(is.na(HRU.output[i, "VEG_CLASS"]), "[NONE]", as.character(vegetation.codes$Bin_type)[HRU.output[i, "VEG_CLASS"] == vegetation.codes$Value])
-
-
+library(dplyr)
 
 ## Read in GIS data except for the LAI *.tif files
 bc.albers <- "+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +datum=NAD83 +units=m +no_defs"
@@ -81,7 +29,7 @@ coords <- coordinates(landcover.ok)
 landcover.values <- values(landcover.ok)
 
 ## Read in all LAI *.tif files
-months <- 1:3
+months <- 1:12
 
 lai.var.names <- paste("lai_", months, sep = "")
 
@@ -105,9 +53,36 @@ colnames(data) <- c("x", "y", lai.var.names)
 
 data$landcover <- landcover.values
 
-library(dplyr)
+## Join vegetation bins (i.e., "Value" from vegetation.codes) to the data object - this allows vegetation bins, rather than raw values to be used for averaging
+data.all <- inner_join(data, vegetation.codes, by = c("landcover" = "Value"))
 
-data %>% group_by(landcover) %>% summarise_all(.funs = c(mean="mean", na.rm = T))
+## Calculate monthly average for all 
+results.mean <- data.all %>% group_by(Bin_type) %>% summarise_at(.vars = c(lai.var.names),
+                                                            .funs = c(mean="mean"), na.rm = T)
+
+results.max <- data.all %>% group_by(Bin_type) %>% summarise_at(.vars = c(lai.var.names),
+                                                                  .funs = c(max="max"), na.rm = T)
+
+max <- data.frame("Bin_type" = results.max$Bin_type, "MAX_LAI" = apply(results.max[,grepl("lai", names(results.max))], 1, max))
+
+exclusions <- c("NO_DATA", "NON_VEGETATED", "SHADOW", "SNOW_ICE", "WATER")
+
+max[max$Bin_type %in% exclusions, "MAX_LAI"] <- 0
 
 
-results <- ddply(data, .(landcover), colwise(mean, na.rm = T))
+
+LAI <- results.mean[,grepl("lai", names(results.mean))] / max$MAX_LAI
+
+is.na(LAI)<-sapply(LAI, is.infinite)
+
+LAI[is.na(LAI)]<-0
+
+LAI$Bin_type <- results.mean$Bin_type
+
+## Plot monthly LAI across different bin types
+require(reshape)
+require(ggplot2)
+
+
+results.melted <- melt(as.data.frame(results.mean, id.vars = "Bin_type"))
+ggplot(results.melted, aes(x = variable, y = value)) + geom_line(aes(color = Bin_type, group = Bin_type))
