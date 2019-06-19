28eb38372a3206a23bdba246c2141f18741fafec
birdl@ae.ca <fykej@ae.ca>
2019-06-19 23:32:52 +0000

    Initial commit of reservoir-rvh-filegenerator.R which is used to generate required *.rvh files for reservoirs included in the model watershed(s). This script is run regardless of whether reservoirs are included within the model watershed; if reservoirs are present, a new reservoirs directory is generated and individual *.rvh files generated for all reservoirs. ReDirectToFile commands are then appended to the end of the main *.rvh file to include reservoirs within the model. If no reservoirs are found within the model watershed, a statement is returned to inform that no reservoirs exist. Preliminary tests successful on June 19, 2019. NOTE: Full testing cannot be completed until updated subbasin raster is received from Dan.

---
+++ raven-input-files/reservoir-rvh-filegenerator.R
@@ -0,0 +1,156 @@
+############################################################################################################################
+##
+## This script generates individual *.rvh files for reservoirs included in a given model run
+##
+## Jun-19-2019
+##
+############################################################################################################################
+
+require(readxl)
+require(RavenR)
+
+#################################################
+##
+## Read-in required datasets and variables
+##
+#################################################
+
+## Read in the subbasin attribute table
+subbasin.codes <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/subbasin_codes.csv")
+
+## Subset to isolate only the rows relevant to the watershed(s) of interest. gsub command removes the " Creek" from GNIS_NAME
+subbasin.subset <- subbasin.codes[gsub( " .*$", "", subbasin.codes$GNIS_NAME) == include.watersheds,]
+
+## Identify all reservoirs/lakes within the watershed(s) of interest
+reservoirs <- unique(subbasin.subset$Res_Lake)
+
+## Remove "<Null>"
+reservoirs <- as.character(reservoirs[!reservoirs %in% "<Null>"])
+
+if(length(reservoirs) <1){print(paste("No reservoirs included within the", include.watersheds, "watershed(s)"))
+  } else {
+
+  ## Read in the rvh file to identify HRU number associated with each reservoir
+  main.HRU.file <- file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), paste(ws.interest, "-", run.number, ".rvh", sep = ""))  
+  
+  HRUs <- rvh.read(main.HRU.file)
+  
+  HRUs <- HRUs$HRUtable
+  
+    ## Create a "reservoirs" folder to house individual *.rvh files for all reservoirs
+  dir.create(file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), "reservoirs"))
+  
+  #################################################
+  ##
+  ## Loop over all reservoirs and generate individual *.rvh files for each reservoir
+  ##
+  #################################################
+  
+  ## For all unique reservoirs, read in the stage-storage and stage-are information
+  for(i in 1:length(reservoirs)){
+    
+    tmp <- read_xlsx("/var/obwb-hydro-modelling/input-data/raw/reservoirs/okanagan-reservoirs.xlsx", sheet = reservoirs[i])
+    
+    ##-----------------------------------------------------------------------------
+    ##
+    ## Extract required weir information and lake depth from tmp
+    ##
+    ##-----------------------------------------------------------------------------
+    
+    parameters <- na.omit(tmp[ ,c("PARAMETER", "VALUE")])
+    
+    ##-----------------------------------------------------------------------------
+    ##
+    ## Isolate the stage-storage curve for the given reservoir and identify the number of points
+    ##
+    ##-----------------------------------------------------------------------------
+    
+    ## Remove "Dead" Storage from the curve, but include zero storage
+    tmp <- tmp[tmp$Current_Storage_dam3 == 0 | tmp$Storage_Type == "Live" | tmp$Storage_Type == "Potential", ]
+    
+    ## Convert volume to m3 as required by Raven
+    tmp$Current_Storage_m3 <- tmp$Current_Storage_dam3 * 1000
+    
+    StageStorage <- tmp[ ,c("Stage_m_GSC", "Current_Storage_m3")]
+    
+    npoints <- nrow(StageStorage)
+    
+    ##-----------------------------------------------------------------------------
+    ##
+    ## Extract the correspinding subbasin ID and HRU ID for the given reservoirs, as well as the lake area
+    ##
+    ##-----------------------------------------------------------------------------
+    
+    SubBasinID <- subbasin.subset[subbasin.subset$Res_Lake == reservoirs[i], "Subbasin_ID.."]
+    
+    HRUID <- HRUs[HRUs$SBID == SubBasinID, "ID"]
+    
+    LakeArea <- HRUs[HRUs$SBID == SubBasinID, "Area"] * (1000*1000)
+    
+    ##-----------------------------------------------------------------------------
+    ##
+    ## Generate individual *.rvh files for all reservoirs within the model watershed
+    ##
+    ##-----------------------------------------------------------------------------
+    
+    ReservoirRVHoutFile <- file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), "reservoirs", paste(reservoirs[i], ".rvh", sep = ""))
+    
+    cat(file=ReservoirRVHoutFile, append=F, sep="",
+        
+        "#########################################################################","\n",
+        "##", "\n",
+        "## Individual Reservoir RVH file. Redirected from Master.rvh file","\n",
+        ":Application       R","\n",
+        ":WrittenBy         Lawrence Bird","\n",
+        ":CreationDate  ",    paste(Sys.time()),"\n",
+        "#---------------------------------------------------------", "\n",
+        "\n",
+        ":Reservoir ", as.character(reservoirs[i]), "\n",
+        ":SubBasinID ", SubBasinID, "\n",
+        ":HRUID ", HRUID, "\n",
+        ":WeirCoefficient ", parameters$VALUE[parameters$PARAMETER == "WeirCoefficient"], "\n",
+        ":CrestWidth ", parameters$VALUE[parameters$PARAMETER == "CrestWidth"], "\n",
+        ":MaxDepth ", parameters$VALUE[parameters$PARAMETER == "MaxDepth"], "\n",
+        ":LakeArea ", LakeArea, "\n",
+        ":AbsoluteCrestHeight ", parameters$VALUE[parameters$PARAMETER == "AbsoluteCrestHeight"], "\n",
+        "\n",
+        ":VolumeStageRelation LOOKUP_TABLE", "\n",
+        npoints, "# number of points in curve", "\n"
+    )
+    
+    write.table(StageStorage, ReservoirRVHoutFile, append = T, col.names = F, row.names = F, sep = ",", quote = F) 
+    
+    cat(file=ReservoirRVHoutFile, append=T, sep="",
+        ":EndVolumeStageRelation", "\n",
+        ":EndReservoir", "\n"
+    )
+    
+    ##-----------------------------------------------------------------------------
+    ##
+    ## Append :RedirectToFile command(s) to the main *.rvh file to point to individual reservoir files
+    ##
+    ##-----------------------------------------------------------------------------
+    
+    if(i == 1){
+      cat(file = main.HRU.file, append = T, sep = "",
+          "\n",
+          "#-------------------------------------------------------", "\n",
+          "# Redirect to Reservoir Files", "\n",
+          "\n",
+          ":RedirectToFile ", ReservoirRVHoutFile
+      )} else {
+        cat(file = main.HRU.file, append = T, sep = "",
+            "\n",
+            ":RedirectToFile ", ReservoirRVHoutFile
+        ) 
+      }
+    
+    
+    
+  } # end for loop
+  
+  print(paste(length(reservoirs), "reservoir(s) included within the", include.watersheds, "watershed(s)"))
+
+} # end if / else
+
+  
