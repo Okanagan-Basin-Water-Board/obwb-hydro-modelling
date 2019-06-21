b4514291946909ebbf069cebf2b2999444487203
birdl@ae.ca <fykej@ae.ca>
2019-06-21 20:04:09 +0000

    Update to rservoir-rvh-file-generator.R to include generation of Ostrich template files, when required. Also inclusion of selection of 'zero' storage_Type tag to ensure the zero storage point is included. Preliminary tests successful on June 21, 2019.

--- raven-input-files/reservoir-rvh-filegenerator.R
+++ raven-input-files/reservoir-rvh-filegenerator.R
@@ -49,7 +49,7 @@ if(length(reservoirs) <1){print(paste("No reservoirs included within the", inclu
   ## For all unique reservoirs, read in the stage-storage and stage-are information
   for(i in 1:length(reservoirs)){
     
-    tmp <- read_xlsx("/var/obwb-hydro-modelling/input-data/raw/reservoirs/okanagan-reservoirs.xlsx", sheet = reservoirs[i])
+    tmp <- read_xlsx("/var/obwb-hydro-modelling/input-data/raw/reservoirs/raven-reservoirs.xlsx", sheet = reservoirs[i])
     
     ##-----------------------------------------------------------------------------
     ##
@@ -66,12 +66,12 @@ if(length(reservoirs) <1){print(paste("No reservoirs included within the", inclu
     ##-----------------------------------------------------------------------------
     
     ## Remove "Dead" Storage from the curve, but include zero storage
-    tmp <- tmp[tmp$Current_Storage_dam3 == 0 | tmp$Storage_Type == "Live" | tmp$Storage_Type == "Potential", ]
+    tmp2 <- tmp[tmp$Storage_Type == "Zero" | tmp$Storage_Type == "Live" | tmp$Storage_Type == "Potential", ]
     
     ## Convert volume to m3 as required by Raven
-    tmp$Current_Storage_m3 <- tmp$Current_Storage_dam3 * 1000
+    tmp2$Current_Storage_m3 <- tmp2$Current_Storage_dam3 * 1000
     
-    StageStorage <- tmp[ ,c("Stage_m_GSC", "Current_Storage_m3")]
+    StageStorage <- tmp2[ ,c("Stage_m_GSC", "Current_Storage_m3")]
     
     npoints <- nrow(StageStorage)
     
@@ -146,11 +146,74 @@ if(length(reservoirs) <1){print(paste("No reservoirs included within the", inclu
       }
     
     
+    ############################################################################################################################
+    ##
+    ## Generate Ostrich Template file for same conditions
+    ##
+    ############################################################################################################################
+    
+    
+    if(run.ostrich == TRUE){
+      
+      calibration.parameter.table <- na.omit(tmp[ ,c("PARAMETER", "VALUE", 'CAL_MIN', "CAL_MAX")])
+      
+      ## convert all columns to character
+      calibration.parameter.table[,] <- lapply(calibration.parameter.table[, ], as.character)
+      
+      ## Create an empty column called CAL_VAR
+      calibration.parameter.table$CAL_VAR <- NA
+      
+      calibration.parameter.table$CAL_VAR[which(!is.na(calibration.parameter.table$CAL_MAX))] <- paste(reservoirs[i], calibration.parameter.table$PARAMETER[which(!is.na(calibration.parameter.table$CAL_MAX))],sep = "_")
+      
+      calibration.parameter.table$CAL_VAR[which(is.na(calibration.parameter.table$CAL_MAX))] <- as.character(calibration.parameter.table$VALUE[which(is.na(calibration.parameter.table$CAL_MAX))])  
+      
+      ##-----------------------------------------------------------------------------
+      ##
+      ## Generate *.rvh.tpl file
+      ##
+      ##-----------------------------------------------------------------------------
+      
+      
+      OstrichReservoirRVHTemplateFile <- file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), "reservoirs", paste(reservoirs[i], ".rvh.tpl", sep = ""))
+      
+      cat(file=OstrichReservoirRVHTemplateFile, append=F, sep="",
+          
+          "#########################################################################","\n",
+          "##", "\n",
+          "## Individual Reservoir RVH file. Redirected from Master.rvh file","\n",
+          ":Application       R","\n",
+          ":WrittenBy         Lawrence Bird","\n",
+          ":CreationDate  ",    paste(Sys.time()),"\n",
+          "#---------------------------------------------------------", "\n",
+          "\n",
+          ":Reservoir ", as.character(reservoirs[i]), "\n",
+          ":SubBasinID ", SubBasinID, "\n",
+          ":HRUID ", HRUID, "\n",
+          ":WeirCoefficient ", calibration.parameter.table$CAL_VAR[calibration.parameter.table$PARAMETER == "WeirCoefficient"], "\n",
+          ":CrestWidth ", calibration.parameter.table$CAL_VAR[calibration.parameter.table$PARAMETER == "CrestWidth"], "\n",
+          ":MaxDepth ", calibration.parameter.table$CAL_VAR[calibration.parameter.table$PARAMETER == "MaxDepth"], "\n",
+          ":LakeArea ", LakeArea, "\n",
+          ":AbsoluteCrestHeight ", parameters$VALUE[parameters$PARAMETER == "AbsoluteCrestHeight"], "\n",
+          "\n",
+          ":VolumeStageRelation LOOKUP_TABLE", "\n",
+          npoints, " # number of points in curve", "\n"
+      )
+      
+      write.table(StageStorage, OstrichReservoirRVHTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F) 
+      
+      cat(file=OstrichReservoirRVHTemplateFile, append=T, sep="",
+          ":EndVolumeStageRelation", "\n",
+          ":EndReservoir", "\n"
+      )
+      
+      if(i == length(reservoirs)){print(paste(length(reservoirs), "required Ostrich template(s) generated..."))}
+    } # End if statement for running Ostrich
+    
     
   } # end for loop
   
   print(paste(length(reservoirs), "reservoir(s) included within the", include.watersheds, "watershed(s)"))
-
+  
 } # end if / else
 
-  
+
