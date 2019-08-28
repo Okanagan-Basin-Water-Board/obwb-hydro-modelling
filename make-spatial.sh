910ce7f2dce60cf88ce4acccaa1e26cf52203a28
birdl@ae.ca <fykej@ae.ca>
2019-08-28 20:53:51 +0000

    Initial commit of make-spatial.sh bash script. This script is used to remap all spatial data to update HRUs and remap climate data accordingly.

---
+++ make-spatial.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo ------------ Generating HRUs --------------------------------
+Rscript hru-generation/hru-generator.R
+echo ------------ Done generating HRUs ---------------------------
+
+
+echo ------------- Writing new Master.rvh file ------------------
+Rscript raven-input-files/rvh-filegenerator.R
+echo --------------Master.rvh has been rewritten ----------------
+
+echo ------------ Running climate grid weighting scripts-----------
+Rscript climate-processing/calculate_hru_weights.r tasmin
+Rscript climate-processing/calculate_hru_weights.r tasmax
+Rscript climate-processing/calculate_hru_weights.r pr
+echo ------------ Done remapping all climate data -----------------
+
+echo ------------ Remapping snow station locations to HRUs --------
+Rscript snow-processing/snow-spatial-analysis.R
+echo ------------ Done remapping snow station locations -----------
+
+echo ------------ All spatial information has been successfully updated ---------
