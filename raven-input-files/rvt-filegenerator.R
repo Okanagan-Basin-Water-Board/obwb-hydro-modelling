51fb6fe1e6c9f311ac328706338992e354703d2a
birdl@ae.ca <fykej@ae.ca>
2019-09-25 23:46:13 +0000

    Update to rvt-filegenerator.R to remove water demand data handling. This handling is now to be completed using owdm-rvt-filegenerator.R

--- raven-input-files/rvt-filegenerator.R
+++ raven-input-files/rvt-filegenerator.R
@@ -203,88 +203,88 @@ cat(file = RVToutFile, append = T, sep = "",
 ##
 ############################################################################################################################
 
-if(include.water.demand == TRUE){
-  
-  RVI.template <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/RVI-Template.csv")
-  
-  RVI.template$PARAMETER <- paste(":", RVI.template$PARAMETER, sep = '')
-  
-  time <- RVI.template[RVI.template$GROUP == "Time", c("PARAMETER", "DEFINITION")]
-  
-  time$DEFINITION <- paste(as.Date(time$DEFINITION, format = "%m/%d/%Y"), "00:00:00", sep = ' ')
-  
-  ## Read in OWDM data (could be one master file for all watersheds/subbasins)
-  owdm <- read.csv("/var/obwb-hydro-modelling/input-data/raw/owdm/Whiteman.csv")
-  
-  owdm$watershed <- gsub( " .*$", "", owdm$subbasin)
-  
-  ## Isolate specified watershed
-  owdm.sub <- owdm[owdm$watershed == include.watersheds,]
-  
-  owdm.sub$extraction.total <- rowSums(owdm.sub[,c("indoor", "outdoor_domestic", "outdoor_animal", "outdoor_other_irrigation")])
-  
-  owdm.sub$date <- paste(owdm.sub$year, owdm.sub$day, sep = "-")
-  
-  owdm.sub$tiso <- as.Date(owdm.sub$date, format = "%Y-%j")
-  
-  model.period.start <- as.Date(time$DEFINITION[time$PARAMETER == ":StartDate"])
-  
-  model.period.end <- as.Date(time$DEFINITION[time$PARAMETER == ":EndDate"])
-  
-  if(nrow(owdm.sub) > 0){
-    
-    subs <- unique(owdm.sub$subbasin_id)
-    
-    for(i in 1:length(subs)){
-      
-      ## isolate extractionf or one subbasin
-      tmp <- owdm.sub[owdm.sub$subbasin_id == subs[i],]
-      
-      warmup.demand.period <- tmp$tiso[1] - model.period.start
-      
-      # If the model is to be run prior to water demand being included, create "empty"/Zero demand for the warmup period
-      if(warmup.demand.period > 0){
-        date.fills <- seq(model.period.start, length.out = warmup.demand.period, by = 1)
-      
-        warmup.demand <- data.frame(matrix(NA, ncol = ncol(tmp), nrow = length(date.fills)))
-        
-        colnames(warmup.demand) <- colnames(tmp)
-        
-        warmup.demand$tiso <- date.fills
-        
-        warmup.demand$extraction.total <- 0
-      
-        tmp <- rbind(warmup.demand, tmp)
-        
-      }
-      
-      ## make extraction total negative to represent "extraction" rather than addition to flow
-      tmp$extraction.total <- tmp$extraction.total * -1
-      
-      ## convert extraction total to m3/s from m3/day
-      tmp$extraction.total <- tmp$extraction.total / (60*60*24)
-  
-      tmp$extraction.total <- ifelse(tmp$extraction.total == -0, 0, tmp$extraction.total)
-      
-      fc <- file(file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), paste(subs[i], "owdm.rvt", sep = "_")), open = "w+")
-      
-      writeLines(sprintf(':BasinInflowHydrograph2 %i # %s',subs[i], paste(subs[i], "owdm.rvt", sep = "_")), fc)
-      writeLines(sprintf('%s 00:00:00 1.0 %i',as.character(tmp$tiso[1]),nrow(tmp)), fc)
-      
-      for (k in 1:nrow(tmp)) {
-        writeLines(sprintf('%g',tmp[k,"extraction.total"]), fc)
-      }
-      
-      writeLines(':EndBasinInflowHydrograph2',fc)
-      close(fc)
-      
-      cat(file = RVToutFile, append = T, sep = "",
-          ":RedirectToFile ", paste(subs[i], "owdm.rvt", sep = "_"), "\n"
-      )
-      
-    }
-    
-  } else {print("No OWDM data exists for currently included watershed(s)...")}
-
-} else {print("Water demand was not included in this model run...")}
+# if(include.water.demand == TRUE){
+#   
+#   RVI.template <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/RVI-Template.csv")
+#   
+#   RVI.template$PARAMETER <- paste(":", RVI.template$PARAMETER, sep = '')
+#   
+#   time <- RVI.template[RVI.template$GROUP == "Time", c("PARAMETER", "DEFINITION")]
+#   
+#   time$DEFINITION <- paste(as.Date(time$DEFINITION, format = "%m/%d/%Y"), "00:00:00", sep = ' ')
+#   
+#   ## Read in OWDM data (could be one master file for all watersheds/subbasins)
+#   owdm <- read.csv("/var/obwb-hydro-modelling/input-data/raw/owdm/Whiteman.csv")
+#   
+#   owdm$watershed <- gsub( " .*$", "", owdm$subbasin)
+#   
+#   ## Isolate specified watershed
+#   owdm.sub <- owdm[owdm$watershed == include.watersheds,]
+#   
+#   owdm.sub$extraction.total <- rowSums(owdm.sub[,c("indoor", "outdoor_domestic", "outdoor_animal", "outdoor_other_irrigation")])
+#   
+#   owdm.sub$date <- paste(owdm.sub$year, owdm.sub$day, sep = "-")
+#   
+#   owdm.sub$tiso <- as.Date(owdm.sub$date, format = "%Y-%j")
+#   
+#   model.period.start <- as.Date(time$DEFINITION[time$PARAMETER == ":StartDate"])
+#   
+#   model.period.end <- as.Date(time$DEFINITION[time$PARAMETER == ":EndDate"])
+#   
+#   if(nrow(owdm.sub) > 0){
+#     
+#     subs <- unique(owdm.sub$subbasin_id)
+#     
+#     for(i in 1:length(subs)){
+#       
+#       ## isolate extractionf or one subbasin
+#       tmp <- owdm.sub[owdm.sub$subbasin_id == subs[i],]
+#       
+#       warmup.demand.period <- tmp$tiso[1] - model.period.start
+#       
+#       # If the model is to be run prior to water demand being included, create "empty"/Zero demand for the warmup period
+#       if(warmup.demand.period > 0){
+#         date.fills <- seq(model.period.start, length.out = warmup.demand.period, by = 1)
+#       
+#         warmup.demand <- data.frame(matrix(NA, ncol = ncol(tmp), nrow = length(date.fills)))
+#         
+#         colnames(warmup.demand) <- colnames(tmp)
+#         
+#         warmup.demand$tiso <- date.fills
+#         
+#         warmup.demand$extraction.total <- 0
+#       
+#         tmp <- rbind(warmup.demand, tmp)
+#         
+#       }
+#       
+#       ## make extraction total negative to represent "extraction" rather than addition to flow
+#       tmp$extraction.total <- tmp$extraction.total * -1
+#       
+#       ## convert extraction total to m3/s from m3/day
+#       tmp$extraction.total <- tmp$extraction.total / (60*60*24)
+#   
+#       tmp$extraction.total <- ifelse(tmp$extraction.total == -0, 0, tmp$extraction.total)
+#       
+#       fc <- file(file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), paste(subs[i], "owdm.rvt", sep = "_")), open = "w+")
+#       
+#       writeLines(sprintf(':BasinInflowHydrograph2 %i # %s',subs[i], paste(subs[i], "owdm.rvt", sep = "_")), fc)
+#       writeLines(sprintf('%s 00:00:00 1.0 %i',as.character(tmp$tiso[1]),nrow(tmp)), fc)
+#       
+#       for (k in 1:nrow(tmp)) {
+#         writeLines(sprintf('%g',tmp[k,"extraction.total"]), fc)
+#       }
+#       
+#       writeLines(':EndBasinInflowHydrograph2',fc)
+#       close(fc)
+#       
+#       cat(file = RVToutFile, append = T, sep = "",
+#           ":RedirectToFile ", paste(subs[i], "owdm.rvt", sep = "_"), "\n"
+#       )
+#       
+#     }
+#     
+#   } else {print("No OWDM data exists for currently included watershed(s)...")}
+# 
+# } else {print("Water demand was not included in this model run...")}
 
