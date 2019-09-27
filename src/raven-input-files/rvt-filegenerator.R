e6edaaa0f98638cfa8babd2fa3d1bb327721ed15
Lawrence <birdl@ae.ca>
2019-09-27 22:43:51 +0000

    Update to rvt.filegenerator to include LinearTransform function and generate required rvt.tpl when run.ostrich == T and is preceipitation linear transform function is specified in the RVP-Template.csv

--- src/raven-input-files/rvt-filegenerator.R
+++ src/raven-input-files/rvt-filegenerator.R
@@ -16,17 +16,30 @@ library(tidyhydat)
 library(RavenR)
 library(ncdf4)
 
-ws.interest <- ws.interest
+## -----------------------------------------------------
+##
+## Read-in RVP template and extract climate correction factors
+##
+## -----------------------------------------------------
 
-include.watersheds <- include.watersheds
+## Read in RVP.template to identify required correction factor for Precipitation
+RVP.template <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/RVP-Template.csv", na.strings = c(""))
 
-run.number <- run.number
+## Make all columns characters
+RVP.template[,] <- lapply(RVP.template.base[, ], as.character)
+
+## Extract the correction factor for Precipitation
+pr.correction <- RVP.template[RVP.template$GROUP == "ClimateParameter" & RVP.template$PARAMETER == "LinearTransform" & RVP.template$DEFINITION == "pr", "VALUE"]
+
+## -----------------------------------------------------
+##
+## Specify multiple commands required to generate WSC rvt files.
+##
+## -----------------------------------------------------
+
 
 ## Specify the location where the HYDAT database is saved
 hydat_here <- "/var/obwb-hydro-modelling/input-data/raw/wsc-hydat/Hydat.sqlite3"
-
-## Read in the list of WSC stations required to be downloaded. This table must also include associated subbasins and watersheds
-# download.list <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/WSC_download_list.csv")
 
 ## Read in the RVI template and identify the start and end dates. WSC data is then only imported for this period. Start.date and end.date
 ## are formatted as required by hy_daily_flows.
@@ -138,17 +151,34 @@ weights <- matrix(nrow = nHRU, ncol = 3, data = c(HRU = HRUs$HRUtable$ID,
 
 RVToutFile <- file.path(output.location, paste(ws.interest, "-", run.number, ".rvt", sep = ""))
 
-cat(file = RVToutFile, append = T, sep = "",
-    "\n",
-    ":StationForcing", "\n",
-    ":ForcingType PRECIP", "\n",
-    ":FileNameNC  ", precip.forcing.filename, "\n",
-    ":VarNameNC   pr", "\n",
-    ":DimNamesNC  HRU time", "\n",
-    ":GridWeights", "\n",
-    ":NumberHRUs  ", nHRU, "\n",
+## If running an OSTRICH calibration, include the :LinearTransform function for precipitation. If not, exclude this command (Expected to slow down model initialization process)
+if(run.ostrich == TRUE & length(pr.correction) == 1){
+  cat(file = RVToutFile, append = T, sep = "",
+      "\n",
+      ":StationForcing", "\n",
+      ":ForcingType PRECIP", "\n",
+      ":FileNameNC  ", precip.forcing.filename, "\n",
+      ":VarNameNC   pr", "\n",
+      ":LinearTransform ", pr.correction, " 0.0", "\n",
+      ":DimNamesNC  HRU time", "\n",
+      ":GridWeights", "\n",
+      ":NumberHRUs  ", nHRU, "\n",
+        ":NumberGridCells ", nGridCell, "\n"
+  )
+  
+} else {
+  cat(file = RVToutFile, append = T, sep = "",
+      "\n",
+      ":StationForcing", "\n",
+      ":ForcingType PRECIP", "\n",
+      ":FileNameNC  ", precip.forcing.filename, "\n",
+      ":VarNameNC   pr", "\n",
+      ":DimNamesNC  HRU time", "\n",
+      ":GridWeights", "\n",
+      ":NumberHRUs  ", nHRU, "\n",
       ":NumberGridCells ", nGridCell, "\n"
-)
+  )
+}
 
 write.table(weights, RVToutFile, append = T, col.names = F, row.names = F, sep = "\t", quote = F)
 
@@ -197,94 +227,105 @@ cat(file = RVToutFile, append = T, sep = "",
     "\n"
 )
 
+
+
 ############################################################################################################################
 ##
-## Grab, process, and generate Raven files for OWDM files for Whiteman Creek
+## Generate *.rvt.tpl file to allow LinearTransform of climate variable(s)
 ##
 ############################################################################################################################
 
-# if(include.water.demand == TRUE){
-#   
-#   RVI.template <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/RVI-Template.csv")
-#   
-#   RVI.template$PARAMETER <- paste(":", RVI.template$PARAMETER, sep = '')
-#   
-#   time <- RVI.template[RVI.template$GROUP == "Time", c("PARAMETER", "DEFINITION")]
-#   
-#   time$DEFINITION <- paste(as.Date(time$DEFINITION, format = "%m/%d/%Y"), "00:00:00", sep = ' ')
-#   
-#   ## Read in OWDM data (could be one master file for all watersheds/subbasins)
-#   owdm <- read.csv("/var/obwb-hydro-modelling/input-data/raw/owdm/Whiteman.csv")
-#   
-#   owdm$watershed <- gsub( " .*$", "", owdm$subbasin)
-#   
-#   ## Isolate specified watershed
-#   owdm.sub <- owdm[owdm$watershed == include.watersheds,]
-#   
-#   owdm.sub$extraction.total <- rowSums(owdm.sub[,c("indoor", "outdoor_domestic", "outdoor_animal", "outdoor_other_irrigation")])
-#   
-#   owdm.sub$date <- paste(owdm.sub$year, owdm.sub$day, sep = "-")
-#   
-#   owdm.sub$tiso <- as.Date(owdm.sub$date, format = "%Y-%j")
-#   
-#   model.period.start <- as.Date(time$DEFINITION[time$PARAMETER == ":StartDate"])
-#   
-#   model.period.end <- as.Date(time$DEFINITION[time$PARAMETER == ":EndDate"])
-#   
-#   if(nrow(owdm.sub) > 0){
-#     
-#     subs <- unique(owdm.sub$subbasin_id)
-#     
-#     for(i in 1:length(subs)){
-#       
-#       ## isolate extractionf or one subbasin
-#       tmp <- owdm.sub[owdm.sub$subbasin_id == subs[i],]
-#       
-#       warmup.demand.period <- tmp$tiso[1] - model.period.start
-#       
-#       # If the model is to be run prior to water demand being included, create "empty"/Zero demand for the warmup period
-#       if(warmup.demand.period > 0){
-#         date.fills <- seq(model.period.start, length.out = warmup.demand.period, by = 1)
-#       
-#         warmup.demand <- data.frame(matrix(NA, ncol = ncol(tmp), nrow = length(date.fills)))
-#         
-#         colnames(warmup.demand) <- colnames(tmp)
-#         
-#         warmup.demand$tiso <- date.fills
-#         
-#         warmup.demand$extraction.total <- 0
-#       
-#         tmp <- rbind(warmup.demand, tmp)
-#         
-#       }
-#       
-#       ## make extraction total negative to represent "extraction" rather than addition to flow
-#       tmp$extraction.total <- tmp$extraction.total * -1
-#       
-#       ## convert extraction total to m3/s from m3/day
-#       tmp$extraction.total <- tmp$extraction.total / (60*60*24)
-#   
-#       tmp$extraction.total <- ifelse(tmp$extraction.total == -0, 0, tmp$extraction.total)
-#       
-#       fc <- file(file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), paste(subs[i], "owdm.rvt", sep = "_")), open = "w+")
-#       
-#       writeLines(sprintf(':BasinInflowHydrograph2 %i # %s',subs[i], paste(subs[i], "owdm.rvt", sep = "_")), fc)
-#       writeLines(sprintf('%s 00:00:00 1.0 %i',as.character(tmp$tiso[1]),nrow(tmp)), fc)
-#       
-#       for (k in 1:nrow(tmp)) {
-#         writeLines(sprintf('%g',tmp[k,"extraction.total"]), fc)
-#       }
-#       
-#       writeLines(':EndBasinInflowHydrograph2',fc)
-#       close(fc)
-#       
-#       cat(file = RVToutFile, append = T, sep = "",
-#           ":RedirectToFile ", paste(subs[i], "owdm.rvt", sep = "_"), "\n"
-#       )
-#       
-#     }
-#     
-#   } else {print("No OWDM data exists for currently included watershed(s)...")}
-# 
-# } else {print("Water demand was not included in this model run...")}
+if(run.ostrich == TRUE){
+
+  
+  climate.parameters <- RVP.template[RVP.template$GROUP == "ClimateParameter", ]
+  
+  if(!all(is.na(climate.parameters$CAL_MAX))){
+
+    climate.parameters.calibrate <- climate.parameters
+  
+    climate.parameters.calibrate$CAL_VAR <- NA
+  
+    climate.parameters.calibrate$CAL_VAR[which(is.na(climate.parameters.calibrate$CAL_VAR))] <- paste(climate.parameters.calibrate$DEFINITION[which(is.na(climate.parameters.calibrate$CAL_VAR))], climate.parameters.calibrate$PARAMETER[which(is.na(climate.parameters.calibrate$CAL_VAR))], sep = "_")
+
+    pr.correction.calibrate <- climate.parameters.calibrate[climate.parameters.calibrate$PARAMETER == "LinearTransform" & climate.parameters.calibrate$DEFINITION == "pr", "CAL_VAR"]
+    
+    #############################################################################################
+    ## 
+    ##  Write *.rvt.tpl file
+    ##
+    #############################################################################################
+    
+    OstrichRVTTemplateFile <- file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), "templates", paste(ws.interest, "-", run.number, ".rvt.tpl", sep = ""))
+    
+    
+    cat(file = OstrichRVTTemplateFile, append = T, sep = "",
+        "\n",
+        ":StationForcing", "\n",
+        ":ForcingType PRECIP", "\n",
+        ":FileNameNC  ", precip.forcing.filename, "\n",
+        ":VarNameNC   pr", "\n",
+        ":LinearTransform ", pr.correction.calibrate, " 0.0", "\n",
+        ":DimNamesNC  HRU time", "\n",
+        ":GridWeights", "\n",
+        ":NumberHRUs  ", nHRU, "\n",
+        ":NumberGridCells ", nGridCell, "\n"
+    )
+    
+    write.table(weights, OstrichRVTTemplateFile, append = T, col.names = F, row.names = F, sep = "\t", quote = F)
+    
+    cat(file = OstrichRVTTemplateFile, append = T, sep = "",
+        ":EndGridWeights", "\n",
+        ":EndStationForcing","\n"
+    )
+    
+    
+    cat(file = OstrichRVTTemplateFile, append = T, sep = "",
+        "\n",
+        ":StationForcing", "\n",
+        ":ForcingType TEMP_MIN", "\n",
+        ":FileNameNC  ", tasmin.forcing.filename, "\n",
+        ":VarNameNC   tasmin", "\n",
+        ":DimNamesNC  HRU time", "\n",
+        ":GridWeights", "\n",
+        ":NumberHRUs  ", nHRU, "\n",
+        ":NumberGridCells ", nGridCell, "\n"
+    )
+    
+    write.table(weights, OstrichRVTTemplateFile, append = T, col.names = F, row.names = F, sep = "\t", quote = F)
+    
+    cat(file = OstrichRVTTemplateFile, append = T, sep = "",
+        ":EndGridWeights", "\n",
+        ":EndStationForcing","\n"
+    )
+    
+    cat(file = OstrichRVTTemplateFile, append = T, sep = "",
+        "\n",
+        ":StationForcing", "\n",
+        ":ForcingType TEMP_MAX", "\n",
+        ":FileNameNC  ", tasmax.forcing.filename, "\n",
+        ":VarNameNC   tasmax", "\n",
+        ":DimNamesNC  HRU time", "\n",
+        ":GridWeights", "\n",
+        ":NumberHRUs  ", nHRU, "\n",
+        ":NumberGridCells ", nGridCell, "\n"
+    )
+    
+    write.table(weights, OstrichRVTTemplateFile, append = T, col.names = F, row.names = F, sep = "\t", quote = F)
+    
+    cat(file = OstrichRVTTemplateFile, append = T, sep = "",
+        ":EndGridWeights", "\n",
+        ":EndStationForcing","\n",
+        "\n"
+    )
+    
+    
+    print("One or more climate parameters will be included in the calibration...")
+  
+  } else {
+    
+    print("No climate parameters will be included in the calibration...")
+    
+  }
+}
 
