4aba02ae08ad9fb37e23c74d54b3febc276c11b7
Lawrence <birdl@ae.ca>
2019-10-21 20:08:19 +0000

    Initial commit of subbasin-properties-generator.R. This file adds SubBasinProperties table to end of the rvh file. Also generates *rvh.tpl file as needed for calibration. This analysis is kept separate from rvh-filegenerator.R as it is needed for each model run, whereas the Master.rvh file is only regenerated when HRUs are updated.

---
+++ src/raven-input-files/subbasin-properties-generator.R
@@ -0,0 +1,259 @@
+#################################################################################################################
+##
+## This script adds SubbasinProperties to the *.rvh file for a particular model run. This process is separated from the 
+## rvh-filegenerator.R due to computational efficieny. The processes within rvh-filegenerator.R take a long time to complete
+## whereas this process is fairly minor and quick. In addition, this process is affected by calibration procedures
+##
+## Oct-18-2019 LAB
+##
+#################################################################################################################
+
+#########################################################
+##
+## Generate the required SubbasinProperties table and append it to the main *.rvh file
+##
+#########################################################
+
+RVP.template.base <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-codes/RVP-Template.csv")
+
+## Isolate only CalibrationGroups and SubbasinProperties
+RVP.template.base <- RVP.template.base[RVP.template.base$GROUP == "CalibrationGroups" | RVP.template.base$GROUP == "SubbasinProperties", ]
+
+
+##-------------------------------------------------------
+##
+## Grouped Calibration Parameters
+##
+##-------------------------------------------------------
+
+## Make a duplicate of the base RVP template to allow it to be recalled when making Ostrich template file
+RVP.template <- RVP.template.base
+
+## Make all columns chracter vectors
+RVP.template[,] <- lapply(RVP.template.base[, ], as.character)
+
+## Subset all grouped calibration parameters
+calibration.specials <- RVP.template[RVP.template$GROUP == "CalibrationGroups", ]
+
+
+## If grouped calibration parameters are presernt, replace the value for each individual parameter with the values specified for the group.
+if(nrow(calibration.specials) > 0){
+  
+  for(i in 1:nrow(calibration.specials)){
+    
+    special_parameter <- calibration.specials[i,]
+    
+    RVP.template[RVP.template$PARAMETER == special_parameter$PARAMETER & RVP.template$VALUE == special_parameter$DEFINITION, "VALUE"] <- special_parameter$VALUE
+    
+  }
+  
+  ## Delete the rows that house the CalibrationGroup definitions
+  RVP.template <- RVP.template[!RVP.template$GROUP == "CalibrationGroups",]
+  
+}
+
+##-------------------------------------------------------
+##
+## Generate the reauired SubbasinProperties Table
+##
+##-------------------------------------------------------
+
+## Isolate only subbasins present within the current model run
+subbasin.properties <- RVP.template[RVP.template$DEFINITION %in% subbasins.present$Subbasin_ID, ]
+
+subbasin.property.names <- names(sort(summary(as.factor(subbasin.properties$PARAMETER)), decreasing = T))
+
+layers <- unique(subbasin.properties$DEFINITION)
+
+subbasin.units <- rep(NA, length(subbasin.property.names))
+
+subbasin.properties.table <- matrix(nrow = length(layers), ncol = length(subbasin.property.names) + 1, NA)
+
+
+## loop over all landuse parameters to populate the landuse.parameter.table
+for(i in 1:length(subbasin.property.names)){
+  
+  para <- subbasin.properties[subbasin.properties$PARAMETER == subbasin.property.names[i],]
+  
+  extract <- para[, c("DEFINITION", "VALUE")]
+  
+  if(i == 1){
+    ## if i == 1, include the "DEFINITION" column as landuse types. if i !=1, only include the "VALUE" column
+    subbasin.properties.table[,1:2] <- as.matrix(extract)
+    
+  } else { 
+    ## Concatenate the extracted value(s) and "_DEFAULT" to fill any missing specified parameter values. This allows not all paramaters to have to be specified for all landuse classes.
+    subbasin.properties.table[,i+1] <- c(as.matrix(extract[,"VALUE"]), rep("_DEFAULT", length(layers) - nrow(extract)))
+  }
+  
+  subbasin.units[i] <- unique(para[,"UNITS"])
+  
+}
+
+
+##-------------------------------------------------------
+##
+## Read in the main *.rvh file to append the subbasin.properties.table to
+##
+##-------------------------------------------------------
+
+## Read in the rvh file to identify HRU number associated with each reservoir
+main.HRU.file <- file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), paste(ws.interest, "-", run.number, ".rvh", sep = ""))  
+
+
+cat(file = main.HRU.file, append = T, sep = "",
+    "\n",
+    "#-------------------------------------------------------", "\n",
+    "#----- Specify Subbasin Properties", "\n",
+    ":SubBasinProperties", "\n",
+    ":Parameters, ", paste(subbasin.property.names, collapse = ", "), "\n",
+    ":Units, ", paste(subbasin.units, collapse = ", "), "\n"
+)
+
+write.table(subbasin.properties.table, main.HRU.file, append = T, col.names = F, row.names = F, sep = ",", quote = F, na = "")
+
+
+cat(file = main.HRU.file, append = T, sep = "",
+    ":EndSubBasinProperties", "\n"
+)
+
+#########################################################
+##
+## If run.ostrich == TRUE, generate an *.rvh.tpl file so subbasin properties are included in calibration
+##
+#########################################################
+
+if(run.ostrich == TRUE){
+  
+  ## Reassign the base RVP.template to ensure that all GROUP labels are captured.
+  RVP.template <- RVP.template.base
+  
+  ## Make all columns characters
+  RVP.template[,] <- lapply(RVP.template[, ], as.character)
+  
+  ## Add empty column to house the calibration variable
+  RVP.template$CAL_VAR <- NA
+  
+  ## Those paramaters which are not included in calibration should have their value assigned to the CAL_VAR column
+  RVP.template[is.na(RVP.template$CAL_MIN), "CAL_VAR"] <- RVP.template$VALUE[is.na(RVP.template$CAL_MIN)]
+  
+  ##-------------------------------------------------------
+  ##
+  ## Subbasin Properties
+  ##
+  ##-------------------------------------------------------
+  
+  ## Test if subbasin.properties has any values in CAL_MAX then it should be included for calibration.
+  ## If NOT all values are NA, generate subbasin.properties.calibrate table and populate.
+  ## If all values ARE NA, 
+  if(!all(is.na(subbasin.properties$CAL_MAX))){
+    
+    subbasins.calibrate <- TRUE
+    
+    ## Isolate only the subasins that are included in the current model run, as well as all CalibrationGroups
+    subbasin.properties.calibrate <- RVP.template[RVP.template$DEFINITION %in% subbasins.present$Subbasin_ID | RVP.template$GROUP == "CalibrationGroups", ]
+    
+    ## concatenate all definitions and parameters to make unique calibration names for each entry
+    subbasin.properties.calibrate$CAL_VAR[which(is.na(subbasin.properties.calibrate$CAL_VAR))] <- paste(subbasin.properties.calibrate$DEFINITION[which(is.na(subbasin.properties.calibrate$CAL_VAR))], subbasin.properties.calibrate$PARAMETER[which(is.na(subbasin.properties.calibrate$CAL_VAR))], sep = "_")
+    
+    ##-------------------------------------------------------
+    ## Grouped Subbasin Properties
+    ##-------------------------------------------------------
+    
+    subbasin.calibration.specials <- subbasin.properties.calibrate[subbasin.properties.calibrate$GROUP == "CalibrationGroups", ]
+    
+    if(nrow(subbasin.calibration.specials) > 0){
+      
+      for(i in 1:nrow(subbasin.calibration.specials)){
+        
+        subbasin.special_parameter <- subbasin.calibration.specials[i,]
+        
+        subbasin.properties.calibrate[subbasin.properties.calibrate$PARAMETER == subbasin.special_parameter$PARAMETER & subbasin.properties.calibrate$VALUE == subbasin.special_parameter$DEFINITION, "CAL_VAR"] <- paste(subbasin.special_parameter$DEFINITION, subbasin.special_parameter$PARAMETER, sep = "_")
+        
+      }
+      
+      subbasin.properties.calibrate <- subbasin.properties.calibrate[!subbasin.properties.calibrate$GROUP == "CalibrationGroups", ]
+      
+    }
+    
+    ##-------------------------------------------------------
+    ## Generate subbasin.properties.table.calibrate
+    ##-------------------------------------------------------
+    
+    subbasin.properties.table.calibrate <- matrix(nrow = length(layers), ncol = length(subbasin.property.names) + 1, NA)
+    
+    
+    for(i in 1:length(subbasin.property.names)){
+      
+      para <- subbasin.properties.calibrate[subbasin.properties.calibrate$PARAMETER == subbasin.property.names[i],]
+      
+      extract <- para[, c("DEFINITION", "CAL_VAR")]
+      
+      if(i == 1){
+        
+        subbasin.properties.table.calibrate[,1:2] <- as.matrix(extract)
+        
+      } else { 
+        
+        subbasin.properties.table.calibrate[,i+1] <- c(as.matrix(extract[,"CAL_VAR"]), rep("_DEFAULT", length(layers) - nrow(extract)))
+      }
+      
+      subbasin.units[i] <- unique(para[,"UNITS"])
+      
+    }
+    
+    print("One or more subbasin property will be included in the calibration...")
+    
+  } else {
+    
+    subbasins.calibrate <- FALSE
+    
+    print("No subbasin properties will be included in the calibration...")
+    
+  }
+  
+
+
+  #############################################################################################
+  ## 
+  ##  Generate *.rvh.tpl file
+  ##
+  #############################################################################################
+  
+  
+  if(subbasins.calibrate == TRUE){
+  
+    ## Re-copy the master *.rvh files to the templates folder. This will be adjusted to operate as an OSTRICH template.
+    file.copy(from = file.path("/var/obwb-hydro-modelling/simulations/Master.rvh"), to = file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), "templates"))
+    
+    ## Renames the above file to be *.rvh.tpl
+    file.rename(from = file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), "templates", "Master.rvh"), to = file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), "templates", paste(ws.interest, "-", run.number, ".rvh.tpl", sep = "")))
+  
+    
+  
+    OstrichRVHTemplateFile <- file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-"), "templates", paste(ws.interest, "-", run.number, ".rvh.tpl", sep = ""))
+    
+    
+    cat(file = OstrichRVHTemplateFile, append = T, sep = "",
+        "\n",
+        "#-------------------------------------------------------", "\n",
+        "#----- Specify Subbasin Properties", "\n",
+        ":SubBasinProperties", "\n",
+        ":Parameters, ", paste(subbasin.property.names, collapse = ", "), "\n",
+        ":Units, ", paste(subbasin.units, collapse = ", "), "\n"
+    )
+    
+    write.table(subbasin.properties.table.calibrate, OstrichRVHTemplateFile, append = T, col.names = F, row.names = F, sep = ",", quote = F, na = "")
+    
+    
+    cat(file = OstrichRVHTemplateFile, append = T, sep = "",
+        ":EndSubBasinProperties", "\n"
+    )
+  
+  }
+
+}
+
+
+
+
