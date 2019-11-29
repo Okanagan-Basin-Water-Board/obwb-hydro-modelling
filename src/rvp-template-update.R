345096b7fcd42546afa815f5039bb830403e81d3
Lawrence <birdl@ae.ca>
2019-11-29 23:59:30 +0000

    Update to rvp-template-update.R to facilitate correct update to RVP and Reservoirs File following a given calibration

--- src/rvp-template-update.R
+++ src/rvp-template-update.R
@@ -8,9 +8,13 @@
 ###########################################################################################################################
 
 require(RavenR)
+require(openxlsx)
+require(tidyr)
+require(plyr)
+
 
 ## Enter the filepath to the calibration attempt that should be used to update the RVP-Template
-calibration.attempt <- "/var/obwb-hydro-modelling/simulations/Preliminary-Natural-Calibration/Preliminary-Natural-Calibration-Sep-16-Natural-Base-Calibration"
+calibration.attempt <- "/var/obwb-hydro-modelling/simulations/Testing-Nov/Testing-Nov-whiteman-test-calibration"
 
 ## Read in the optimum parameter values identified by Ostrich (i.e., these are automatically compiled into OstModel0.txt)
 data <- Ost.read(file.path(calibration.attempt, "OstModel0.txt"))
@@ -29,13 +33,80 @@ RVP.template <- read.csv("/var/obwb-hydro-modelling/input-data/raw/parameter-cod
 
 
 
-## Convert the optimum parameters to characters and remove the first column (i.e., the Objective Function)
-replace <- as.character(t(optim0[,-1]))
+# ## Convert the optimum parameters to characters and remove the first column (i.e., the Objective Function)
+# replace <- as.character(t(optim0[,-1]))
+# 
+# ## Convert the "Value" column in the RVP.Template to character vector
+# RVP.template$VALUE <- as.character(RVP.template$VALUE)
+# 
+# ## Assign the optimum parameters to the RVP.Template. The order should match since they originated from this file.
+# RVP.template[!is.na(RVP.template$CAL_MIN), "VALUE"] <- replace
+# 
+# write.csv(RVP.template, "/var/obwb-hydro-modelling/input-data/raw/parameter-codes/RVP-Template.csv", row.names = F, quote = F, na = "")
 
-## Convert the "Value" column in the RVP.Template to character vector
-RVP.template$VALUE <- as.character(RVP.template$VALUE)
 
-## Assign the optimum parameters to the RVP.Template. The order should match since they originated from this file.
-RVP.template[!is.na(RVP.template$CAL_MIN), "VALUE"] <- replace
+## Create a "Name" column to house the parameter name of the calibration parameters - this is used to join the dataframes
+RVP.template$Name <- paste(RVP.template$DEFINITION, RVP.template$PARAMETER, sep = "_")
 
-write.csv(RVP.template, "/var/obwb-hydro-modelling/input-data/raw/parameter-codes/RVP-Template.csv", row.names = F, quote = F, na = "")
+## Convert the OSTRICH output to "long" format
+x <- gather(optim0)
+
+## For "DEFAULT" parameter names, replace the "X.DEFAULT." with "[DEFAULT]" to ensure the join is successful - the name is changed during read in to R
+x$key <- gsub("X.DEFAULT.", "[DEFAULT]", x$key)
+
+## Change the column names of x so that there is a common name to join on (i.e., "Name")
+colnames(x) <- c("Name", "value")
+
+## Join the two dataframes
+y <- join(RVP.template, x, by = "Name", type = "left")
+
+## Convert all elements of the RVP.template object to character (by default, some are factors)
+RVP.template[, ] <- lapply(RVP.template[, ], as.character)
+
+## Replace values that were calibrated (i.e., anything that isn't NA) in the RVP.template object
+RVP.template[which(!is.na(y$value)), "VALUE"] <- y[which(!is.na(y$value)), "value"]
+
+
+
+###########################################################################################################################
+##
+## Reservoirs
+
+## Enter the filepath to the calibration attempt that should be used to update the RVP-Template
+calibration.attempt <- "/var/obwb-hydro-modelling/simulations/Natural-Calibration/Natural-Calibration-Oct-15-Reservoir-Calibration-Test"
+
+
+## Read in the optimum parameter values identified by Ostrich (i.e., these are automatically compiled into OstModel0.txt)
+data <- Ost.read(file.path(calibration.attempt, "OstModel1.txt"))
+
+## Confirm that there is only one parameter set in the OstModel0 file. If not, this will identify the best set (based on the Objectve Function).
+optim0 <- Ost.bestparams(Ost.data = data, best.num = 0.0001)
+
+x <- gather(optim0)
+
+reservoir_param <- grep("_CrestWidth", x$key, value = FALSE)
+
+reservoirs <- x[reservoir_param, ]
+
+reservoir.names <- gsub("_", " ", gsub("_CrestWidth", "", reservoirs$key))
+
+reservoirs$value <- reservoirs$value * 2
+
+
+for(i in 1:length(reservoir.names)){
+  
+  
+  current.reservoir <- read.xlsx("/var/obwb-hydro-modelling/input-data/raw/reservoirs/raven-reservoirs.xlsx", sheet = reservoir.names[i])
+  
+  current.reservoir[which(current.reservoir$PARAMETER == "CrestWidth"), "VALUE"] <- reservoirs$value[i]
+  
+  wb <- loadWorkbook("/var/obwb-hydro-modelling/input-data/raw/reservoirs/raven-reservoirs.xlsx")
+  
+  writeData(wb, sheet = reservoir.names[i], current.reservoir, colNames = TRUE, rowNames = FALSE)
+  
+  saveWorkbook(wb, "/var/obwb-hydro-modelling/input-data/raw/reservoirs/raven-reservoirs.xlsx", overwrite = TRUE)
+  
+}
+
+
+
