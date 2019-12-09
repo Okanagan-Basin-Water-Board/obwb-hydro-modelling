8aefbfd77e81f449fc33aaf7aa8b6978ed83b8bc
Lawrence <birdl@ae.ca>
2019-12-09 18:51:57 +0000

    Updated to include ONE Tied Parameter - this ensures that Field Capacity is always above Sat Wilt. However, implementation only exists for this to be included as ONE paramater (i.e., default value). For loops needs to be included if these values should be adjusted per soil layer.

--- src/ostrich-file-generator.R
+++ src/ostrich-file-generator.R
@@ -310,6 +310,42 @@ col.constraint <- which(colnames(diag) == "DIAG_PCT_BIAS")
 
 
 
+
+#####################################
+## Begin Tied Parameters (i.e., Field Capacity / Sat Wilt)
+####################################
+
+
+tied.parameter.loc <- which(parameter.table[,1] %like% "FIELD_CAPACITY")
+
+tied.parameters <- parameter.table[tied.parameter.loc, 1]
+
+
+parameter.table[tied.parameter.loc, 1] <- "FIELD_CAPACITY_X_PARAM"
+
+
+tied.parameter.table <- matrix(nrow = 1, ncol = 9)
+
+
+tied.parameter.table[1,1] <- tied.parameters[1]
+
+tied.parameter.table[1,2] <- 2
+
+tied.parameter.table[1,3] <- "[DEFAULT]_SAT_WILT"
+
+tied.parameter.table[1,4] <- "FIELD_CAPACITY_X_PARAM"
+
+tied.parameter.table[1,5] <- "linear"
+
+tied.parameter.table[1,6] <- 0.0
+
+tied.parameter.table[1,7] <- 1.0
+
+tied.parameter.table[1,8] <- 1.0
+
+tied.parameter.table[1,9] <- 0.0
+
+
 ###################################################################
 ##
 ## Write OstIn.txt File
@@ -373,6 +409,19 @@ write.table(parameter.table, OSTInFile, append = T, col.names = F, row.names = F
 
 cat(file = OSTInFile, append = T, sep = "",
     "EndParams", "\n",
+    "\n",
+    "#---------------------------------------------------------", "\n",
+    "# ---- Define Tied Parameters -------------------------", "\n",
+    "\n",
+    "BeginTiedParams",
+    "\n"
+)
+
+write.table(tied.parameter.table, OSTInFile, append = T, col.names = F, row.names = F, sep = "\t", quote = F)
+
+cat(file = OSTInFile, append = T, sep = "",
+    "\n",
+    "EndTiedParams",
     "\n",
     "#---------------------------------------------------------", "\n",
     "# ---- Define Response Variables -------------------------", "\n",
