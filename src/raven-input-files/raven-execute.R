1a05f602f699a74d298d1a3b8514fdcdbe68cbb9
Lawrence <birdl@ae.ca>
2020-01-22 19:27:27 +0000

    Updates to raven-execute.R to facilitate testing of OSTRICH updates and residual streamflow modelling

--- src/raven-input-files/raven-execute.R
+++ src/raven-input-files/raven-execute.R
@@ -19,25 +19,25 @@ cores <- detectCores() - 1
 ptm <- proc.time()
 
 ## Specify the name to be associated with output files - note that this could be "Multi" if multiple watersheds to be modelled
-ws.interest <- "Operation-test"
+ws.interest <- "Residual-Tests"
 
 ## Specify the watersheds to be modelled. If multiple, generate a string using c("WS1", "WS2"...WSn")
 # include.watersheds <- c("Coldstream", "Equesis", "Inkaneep", "McDougall", "McLean", "Mill", "Mission", "Naramata", "Naswhito", "Penticton", "Powers", "Shingle", "Shorts", "Shuttleworth", "Trepanier", "Trout", "Vaseux", "Vernon", "Whiteman")
 # include.watersheds <- c("Whiteman", "Trout", "Coldstream", "Vaseux")
-include.watersheds <- "Powers"
+include.watersheds <- c("Mission", "Vernon", "Whiteman", "Coldstream")
 
 ## Specify a run number to associated with outputs
-run.number <- "powers-div-create"
+run.number <- "multiple-ostrich"
 
 ## Add comments to README file.
-run.comments <- "Testing operation of Powers Creek Diversion Estimates"
+run.comments <- "Test updated calibration select"
 
 ## Specify individual subbasins that should be disabled (e.g., Lambly Lake & Contributing area under natural conditions, and all diversions)
 # disable.subbasins <- c(2407, 2408, 2423, 2422, 2421, 1421, 256)
-disable.subbasins <- c()
+disable.subbasins <- c(2423, 2422, 2421, 1421, 256) # For Residuals, only disable the diversions.
 
 ## Specify whether Ostrich templates and input files should be written for this run
-run.ostrich <- FALSE
+run.ostrich <- TRUE
 
 ## Specify whether the model is being run for validation purposes
 validate.model <- FALSE
@@ -55,10 +55,10 @@ calibrate.reservoirs <- FALSE
 manage.reservoirs <- FALSE
 
 ## Should soil thicknesses be calibrated?
-calibrate.soil.thicknesses <- FALSE
+calibrate.soil.thicknesses <- TRUE
 
 ## Should diversion volumes for the given watershed(s) be calculated?
-calculate.diversions <- TRUE
+calculate.diversions <- FALSE
 
 ## Define the period of calibration / diagnostics
 calibration.start <- "1996-01-01"
@@ -253,9 +253,9 @@ if(run.ostrich == TRUE & exists("stations.included") == TRUE){
   ## Request user input on which WSC station the model should be calibrated to.
   source("/var/obwb-hydro-modelling/src/calibration-select.R")
   # 
-  calibration.stations <- c("Whiteman_Nat_Q", "Whiteman_Nat_Q")
+  # calibration.stations <- c("Whiteman_Nat_Q", "Whiteman_Nat_Q")
   # 
-  calibration.station.weights <- c(0.5, 0.5)
+  # calibration.station.weights <- c(0.5, 0.5)
   
   ## set working directory to current model run directory
   setwd(file.path("/var/obwb-hydro-modelling/simulations", ws.interest, paste(ws.interest, run.number, sep = "-")))
