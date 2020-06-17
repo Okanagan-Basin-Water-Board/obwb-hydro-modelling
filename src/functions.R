92889f0f89c42381a808f7d641bdddd67200fea0
Lawrence <birdl@ae.ca>
2020-06-17 19:17:57 +0000

    Source for getmode() function included as comment. R does not have a built in mode function, so a user-defined function is needed.

--- src/functions.R
+++ src/functions.R
@@ -2,6 +2,7 @@
 source("/var/obwb-hydro-modelling/file-config.R")
 
 # function to find mode.
+# https://www.tutorialspoint.com/r/r_mean_median_mode.htm
 getmode <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
